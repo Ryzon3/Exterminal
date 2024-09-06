0398f0bef524efa7a972423eadbb38f3354f86ee
Ryan Cole <ryanmcole20@gmail.com>
2024-09-06 17:40:38 -0400

    Fixed autosave and cleaned up for loop slightly

--- main.py
+++ main.py
@@ -102,6 +102,94 @@ def change_directory(new_dir):
     except PermissionError:
         return f"You do not have permission to access '{new_dir}'"
 
+def execute(console, command, messages):
+    # run the command
+    console.print(f"[dodger_blue1]Running command:[/dodger_blue1] [hot_pink2]{command}[/hot_pink2]")
+    # Run command here using subprocess and capture output
+    with console.status("Output: \n", spinner="bouncingBall", spinner_style="hot_pink2"):
+        result = subprocess.run(command, shell=True, capture_output=True)
+    # Check if there was an error
+    if result.returncode != 0:
+        # If there is an error pause execution and ask if user would like to auto fix or not
+        error = result.stderr.decode("utf-8")
+        return error
+    result = result.stdout.decode("utf-8")
+    if result == "":
+        result = "Command executed successfully."
+    console.print(f"[dodger_blue1]Output:[/dodger_blue1]\n[hot_pink2]{result}[/hot_pink2]")
+    messages.append({'role': 'assistant', 'content': f"COMMAND: {command}\nOUTPUT: {result}"})
+    return None
+
+def fixError(console, command, messages, error, client, i):
+    # Pause and ask user if they would like to auto fix the error
+    console.print(f"[bright_red]Error:[/bright_red] {error}")
+    ans = console.input("[dodger_blue1]An error occured while running the command.\nWould you like to auto fix it?([green1]y[/green1]/[bright_red]n[/bright_red])[/dodger_blue1]")
+    if 'n' in ans.lower():
+        return None
+    if 'y' in ans.lower():
+        # Auto fix the error
+        console.print("[dodger_blue1]Auto fixing the error...[/dodger_blue1]")
+        
+        # now we attempt to fix the error using llm.
+        # We will make a custom system prompt, and pass in the previous LLMs thoughts and commands
+        # If the error is fixable by just changing the current and future commands, we will do that
+        # If the error otherwise requires us to change past commands or its a user error then we can't fix it
+        # Thus for this case we will tell user sorry we can't fix this error with the current information
+        
+        prompt = '''
+        You are a repair technician for Exterminal. Your job is to fix any errors that occur while running commands in the terminal.
+        YOu will only be able to fix errors that are caused by the commands themselves, not user errors.
+        You can only fix errors that are caused by the current command or future commands, not past commands.
+        If the error is not fixable then you will instead only output 1 command that is a "ANSWER:" command explaining it.
+        You will be given 3 things as input:
+        1. The error that occurred while running the command.
+        2. The command that caused the error.
+        3. The exterminal system's thoughts and command chains.
+        
+        You will output a json object with the following format:
+        {
+            "thoughts": "Your thoughts on the error and how to fix it",
+            "commands" (Copy all LLM commands but only change the current or future commands): [
+                Example1-> "EXECUTE: command1",
+                Example2-> "NOINFO:",
+                Example3-> "EXECUTE AND CONFIRM: command2",
+            ]
+        }
+        '''
+        
+        # Now we will make message objects for the LLM to use
+        nmessages = [
+            {'role': 'system', 'content': prompt},
+            {'role': 'user', 'content': messages[-1]['content']},
+            {'role': 'user', 'content': error},
+            {'role': 'user', 'content': "CURRENT COMMAND THAT WAS RUN> " + command},
+        ]
+        
+        # Now we will query the LLM to fix the error
+        with console.status("Querying LLM to fix error...", spinner="bouncingBall", spinner_style="hot_pink2"):
+            response = client.chat.completions.create(
+                model='gpt-4o-2024-08-06',
+                response_format={ "type": "json_object" },
+                messages=nmessages,
+                temperature=0
+            )
+            # Parse output
+            output = response.choices[0].message.content
+            output = json.loads(output)
+        console.print(output)
+        n_command = output['commands'][i]
+        if "ANSWER:" in n_command:
+            console.print("[dodger_blue1]Sorry, I can't fix this error with the current information. Please try a different command or provide more information.[/dodger_blue1]")
+            return None
+        
+        idx = n_command.find(":")
+        n_command = n_command[idx+2:]
+        error = execute(console, n_command, messages)
+        if not error:
+            # it worked! So lets return the new commands
+            return output['commands']
+        return error
+
 
 if __name__ == '__main__':
     load_dotenv()
@@ -168,11 +256,19 @@ if __name__ == '__main__':
             console.print("[dodger_blue1]You can also type '[u bright_red]exit[/u bright_red]' to exit [b hot_pink2]Exterminal[/b hot_pink2] or '[u bright_red]clear[/u bright_red]' to clear the terminal.[/dodger_blue1]")
             console.print("")
             continue
+        skip_cache = False
+        output = None
+        
+        if '--force-llm' in inp:
+            # Remove the flag and force LLM query instead of using cache
+            inp = inp.replace('-force-llm', '')
+            skip_cache = True
         
         msg = "WORLD_MODEL:\n" + str(world_model) + "\n\n\n" + "USER_INPUT" + inp
         messages.append({'role': 'user', 'content': inp})
         
-        output = cache_manager.get_cached_response(inp)
+        if not skip_cache:
+            output = cache_manager.get_cached_response(inp)
         
         if not output:
             with console.status("Querying LLM...", spinner="bouncingBall", spinner_style="hot_pink2"):
@@ -191,9 +287,12 @@ if __name__ == '__main__':
         if 'world_model' in output:
             world_model.update(output['world_model'])
         
-        error = None
+        escape = False
         # Get all "EXECUTE" commands and run them
-        for command in output['commands']:
+        i = -1
+        while i < len(output['commands']) - 1:
+            i += 1
+            command = output['commands'][i]
             if "EXECUTE" in command and 'cd' in command:
                 command = command.replace("EXECUTE: ", "")
                 command = command.replace("EXECUTE AND CONFIRM: ", "")
@@ -205,21 +304,15 @@ if __name__ == '__main__':
                     messages.append({'role': 'assistant', 'content': f"COMMAND: {command}\nOUTPUT: {result}"})
             elif "EXECUTE:" in command:
                 command = command.replace("EXECUTE: ", "")
-                console.print(f"[dodger_blue1]Running command:[/dodger_blue1] [hot_pink2]{command}[/hot_pink2]")
-
-                # Run command here using subprocess and capture output
-                with console.status("Output: \n", spinner="bouncingBall", spinner_style="hot_pink2"):
-                    result = subprocess.run(command, shell=True, capture_output=True)
-                # Check if there was an error
-                if result.returncode != 0:
-                    # If there is an error pause execution and ask if user would like to auto fix or not
-                    error = result.stderr.decode("utf-8")
-                    break
-                result = result.stdout.decode("utf-8")
-                if result == "":
-                    result = "Command executed successfully."
-                console.print(f"[dodger_blue1]Output:[/dodger_blue1]\n[hot_pink2]{result}[/hot_pink2]")
-                messages.append({'role': 'assistant', 'content': f"COMMAND: {command}\nOUTPUT: {result}"})
+                error = execute(console, command, messages)
+                while error:
+                    error = fixError(console, command, messages, error, client, i)
+                    if error is None:
+                        escape = True
+                        break
+                    if type(error) == list:
+                        output['commands'] = error
+                        break
             elif "ANSWER:" in command:
                 command = command.replace("ANSWER: ", "")
                 console.print(f"[hot_pink2]{command}[/hot_pink2]")
@@ -237,28 +330,17 @@ if __name__ == '__main__':
                     # skip the command
                     messages.append({'role': 'assistant', 'content': f"SKIPPED: {command}"})
                     continue
-                # run the command
-                console.print(f"[dodger_blue1]Running command:[/dodger_blue1] [hot_pink2]{command}[/hot_pink2]")
-                # Run command here using subprocess and capture output
-                with console.status("Output: \n", spinner="bouncingBall", spinner_style="hot_pink2"):
-                    result = subprocess.run(command, shell=True, capture_output=True)
-                # Check if there was an error
-                if result.returncode != 0:
-                    # If there is an error pause execution and ask if user would like to auto fix or not
-                    error = result.stderr.decode("utf-8")
-                    break
-                result = result.stdout.decode("utf-8")
-                if result == "":
-                    result = "Command executed successfully."
-                console.print(f"[dodger_blue1]Output:[/dodger_blue1]\n[hot_pink2]{result}[/hot_pink2]")
-                messages.append({'role': 'assistant', 'content': f"COMMAND: {command}\nOUTPUT: {result}"})
-        if error:
-            # Pause and ask user if they would like to auto fix the error
-            console.print(f"[bright_red]Error:[/bright_red] {error}")
-            ans = console.input("[dodger_blue1]An error occured while running the command.\nWould you like to auto fix it?([green1]y[/green1]/[bright_red]n[/bright_red])[/dodger_blue1]")
-            if 'y' in ans.lower():
-                # Auto fix the error
-                console.print("[dodger_blue1]Auto fixing the error...[/dodger_blue1]")
+                error = execute(console, command, messages)
+                while error:
+                    error = fixError(console, command, messages, error, client, i)
+                    if error is None:
+                        escape = True
+                        break
+                    if type(error) == list:
+                        output['commands'] = error
+                        break
+            if escape:
+                break
         
         if int(time.time()) == 0:
             cache_manager.remove_expired_entries()
